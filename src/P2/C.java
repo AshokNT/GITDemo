bc0136aa1f89c46f9c91d8234fe04e7065b7a948
Ashok N T <ANT2@dsone.3ds.com>
2018-05-31 18:14:36 +0530

    reverting the changes

--- src/P2/C.java
+++ src/P2/C.java
@@ -6,7 +6,7 @@ public class C {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Line added while deleting filesin C");
+		// TODO Auto-generated method stub
 
 	}
 
--- src/P2/D.java
+++ src/P2/D.java
@@ -6,8 +6,7 @@ public class D {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		System.out.println("Line added while deleting files in D");
+		// TODO Auto-generated method stub
 
 	}
 
